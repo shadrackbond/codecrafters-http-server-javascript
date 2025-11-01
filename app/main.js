0332a08874dd8292f858a72dc6bf5a7b3cce8c1c
shadrackbond <stellamusembi10@gmail.com>
2025-11-01 10:34:41 +0300

    Getting a get request of /index.html and if path 200 if not 404

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const path = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -8,7 +9,12 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  let PATH = "http://localhost:4221/index.html";
+  if(PATH){
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  }else{
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+  }
 });
 
 server.listen(4221, "localhost");
