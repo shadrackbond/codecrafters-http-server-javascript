3c75aad73295eb59629e8ea91fbe41d0c3de0028
shadrackbond <stellamusembi10@gmail.com>
2025-11-03 10:06:19 +0300

    Getting a get request of /index.html and if path 200 if not 404

--- app/main.js
+++ app/main.js
@@ -4,21 +4,43 @@ const path = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("connect", () => {
+  socket.on("data", (data) => {
+    const requestString = data.toString();
+    console.log("Received request: \n", requestString);
+
+    const requestLine = requestString.split("\r\n"[0]);
+    const parts = requestLine.split(" ");
+
+    if(parts.length < 2){// to make sure the request line is valid if not close the connection
+      socket.end()
+      return;
+    }
+
+    const urlPath = parts[1];
+
+    if(urlPath === '/' || urlPath === '/index.html'){
+      console.log("sending 200 OK")
+      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    }
+    else{
+      console.log("sending 404 Not Found");
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    }
+
+    socket.end()// closing the connection after sending the response
   });
-  const urlPath = window.location.protocol + "//" + window.location.host + window.location.pathname;
-  console.log(urlPath);
-  if(urlPath.includes('index.html')|| urlPath.endsWith('/')){
-    socket.write("HTTP/1.1 200 OK\r\n\r\n")
-  }
-  else{
-    socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
-  }
-  
+  socket.on("close", () =>{
+    console.log("connection closed");
+  });
+
+  socket.on("error", (err) =>{
+    console.log("socket error: ", err.message)
+  })
 });
 
 
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", ()=>{
+  console.log("Server listening on http://localhost:4221")
+});
